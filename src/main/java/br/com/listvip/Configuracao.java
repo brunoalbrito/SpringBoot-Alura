23e063990810ff0956d1aff6c4e20e1d17e8ca07
bruno <brunoalbuquerquebrito@outlook.com>
2018-12-31 10:08:08 -0200

    Adicionando engine Thymeleaf, pagina html para responder a requisicao da nova Controller criada

--- src/main/java/br/com/listvip/Configuracao.java
+++ src/main/java/br/com/listvip/Configuracao.java
@@ -3,18 +3,18 @@ package br.com.listvip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
 public class Configuracao {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String ola(){
-        return "Olá, bem vindo ao sistema Lista VIPs";
-    }
+//    @RequestMapping("/")
+//    @ResponseBody
+//    String ola(){
+//        return "Olá, bem vindo ao sistema Lista VIPs";
+//    }
 
 
     public static void main(String[] args) {
---
+++ src/main/java/br/com/listvip/ConvidadoController.java
@@ -0,0 +1,14 @@
+package br.com.listvip;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ConvidadoController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
