43695c983218feb829df3068f7309cdabe1b045e
bruno <brunoalbuquerquebrito@outlook.com>
2019-01-01 21:30:45 -0200

    Atualizando coesao do banco com a aplicacao

--- db.listavip.sql
+++ db.listavip.sql
@@ -1,11 +1,13 @@
 CREATE DATABASE listavip;
 USE listavip;
 
-CREATE TABLE convidados(id INT NOT NULL AUTO_INCREMENT, 
+CREATE TABLE convidado(id INT NOT NULL AUTO_INCREMENT, 
 nome varchar(25),
 email varchar(40),
 telefone int,PRIMARY KEY(id));
 
-INSERT INTO convidados(nome,email,telefone)VALUES('Bruno','bruno@bruno.com', 999999999);
+INSERT INTO convidado(nome,email,telefone)VALUES('Bruno','bruno@bruno.com', 999999999);
 
-SELECT * FROM convidados;
+SELECT * FROM convidado;
+
+DROP TABLE convidado;
